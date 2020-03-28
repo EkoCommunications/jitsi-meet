b0dea599558fc18dbbe87a77d6b0bd1275c05f95
Eakawat Tantamjarik <eakawat2232@gmail.com>
2020-03-29 00:22:51 +0700

    Remove invite button from the call

--- react/features/toolbox/components/native/OverflowMenu.js
+++ react/features/toolbox/components/native/OverflowMenu.js
@@ -11,7 +11,6 @@ import { IconDragHandle } from '../../../base/icons';
 import { connect } from '../../../base/redux';
 import { StyleType } from '../../../base/styles';
 import { SharedDocumentButton } from '../../../etherpad';
-import { InviteButton } from '../../../invite';
 import { AudioRouteButton } from '../../../mobile/audio-mode';
 import { LiveStreamButton, RecordButton } from '../../../recording';
 import { RoomLockButton } from '../../../room-lock';
@@ -127,7 +126,6 @@ class OverflowMenu extends PureComponent<Props, State> {
                 onSwipe = { this._onSwipe }
                 renderHeader = { this._renderMenuExpandToggle }>
                 <AudioRouteButton { ...buttonProps } />
-                <InviteButton { ...buttonProps } />
                 <AudioOnlyButton { ...buttonProps } />
                 <RaiseHandButton { ...buttonProps } />
                 <MoreOptionsButton { ...moreOptionsButtonProps } />
